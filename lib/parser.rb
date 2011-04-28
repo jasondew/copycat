421bdefd3d05764db4b1749956ee278d4ca999d7
jed <jed@seymour.(none)>
2011-04-28 18:30:48 -0400

    Call parser through JRuby instead of shelling out (much faster)

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,6 +1,42 @@
+require 'java'
+require 'lib/java/stanford-parser.jar'
+import 'edu.stanford.nlp.parser.lexparser.LexicalizedParser'
+import 'edu.stanford.nlp.parser.lexparser.Options'
+import 'edu.stanford.nlp.process.DocumentPreprocessor'
+import 'java.io.StringReader'
+
 module Parser
-  PARSER_DIR = '/home/jed/stanford-parser-2011-04-20'
+  PARSER_DEFINITION = 'englishPCFG.ser.gz'
+
   def self.parse(s)
+    op = Options.new
+    @lp ||= LexicalizedParser.new(PARSER_DEFINITION, op)
+    op = @lp.op
+    # op.tlpParams.setInputEncoding
+    tlp = op.tlpParams.treebankLanguagePack
+    tokenizer = tlp.getTokenizerFactory
+    # The java code sets a lot of other options here on the dp object,
+    # but it seems to work without doing that, so why do anything else?
+    dp = DocumentPreprocessor.new(StringReader.new(s))
+    dp.setTokenizerFactory(tokenizer)
+    it = dp.iterator
+    sentences = []
+    sentences << it.next while it.hasNext
+    if(sentences.length != 1)
+      raise "ERROR: Tokenized #{sentences.length} sentences!"
+    end
+    @lp.parse(sentences[0])
+    tree = @lp.getBestParse
+    java_tree_to_arrays(tree)
+  end
+
+  def self.java_tree_to_arrays(tree)
+    kids = tree.children.map{|x|java_tree_to_arrays(x)}
+    kids.unshift(tree.label.value)
+    kids
+  end
+
+  def self.shell_out_parse(s)
     fn = "/tmp/parsee-#{(10**20).to_s(36)}"
     open(fn,'w'){|f|f.write(s)}
     res = `#{PARSER_DIR}/lexparser.csh #{fn} 2>/dev/null`
