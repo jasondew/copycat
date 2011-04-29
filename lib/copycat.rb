5bdb087a80fa4bd766e2a03d3aec83f6967eb9da
Team JED <jed@seymour.cse.sc.edu>
2011-04-29 12:08:35 -0400

    First draft of copycat

--- lib/copycat.rb
+++ lib/copycat.rb
@@ -1,3 +1,107 @@
 require "copycat/version"
+
+require "tree"
 require "parser"
 require "wordnet"
+
+module Copycat
+
+  module ClassMethods
+    def compare(sentence1, sentence2)
+      tree1 = Parser.parse(sentence1)
+      tree2 = Parser.parse(sentence2)
+      transform_tree tree1
+      transform_tree tree2
+
+      compare_subtrees tree1, tree2
+
+#     subtrees1 = assemble_subtrees tree1
+#     subtrees2 = assemble_subtrees tree2
+#
+#     nonterminal_types = (subtrees1.keys & subtrees2.keys)
+#
+#     nonterminal_types.each do |nonterminal_type|
+#       # Loop through all pairs of subtrees for this nonterminal type
+#       subtrees1[nonterminal_type].each do |t1|
+#         subtrees2[nonterminal_type].each do |t2|
+#           # some comparison of t1 and t2
+#         end
+#       end
+#     end
+    end
+
+    def compare_subtrees tree1, tree2
+      if(tree1.size == 2 and tree2.size == 2)
+        # base case, we have two collections of wordnet objects
+        return wordnet_compare(tree1.children[0].content, tree2.children[0].content)
+      end
+
+      tree1_child_types = tree1.children.map &:content
+      tree2_child_types = tree2.children.map &:content
+
+      # crude initial method -- return 0 if they don't have exactly the
+      # same immediate substructure
+      return 0 if tree1_child_types != tree2_child_types
+
+      total = 0.0
+      tree1.children.zip(tree2.children).each do |stree1, stree2|
+        total += compare_subtrees(stree1, stree2)
+      end
+      total / tree1.children.length
+    end
+
+    def wordnet_compare(entries1, entries2)
+      if(entries1.class == String or entries2.class == String)
+        return entries1 == entries2 ? 1 : 0
+      end
+      min_distance = 30.0 
+      entries1.each do |entry1|
+        entries2.each do |entry2|
+          hdf = entry1.hypernym_distance_from entry2
+          min_distance = [min_distance, hdf].min if hdf
+        end
+      end
+      # There's probably a better way to convert the distance to a similarity
+      # measure. Probably.
+      1 - min_distance / 30.0
+    end
+          
+
+    # Groups the subtrees by non-terminal type
+    def assemble_subtrees tree
+      subtrees = {}
+      tree.each do |t|
+        next if t.children.empty?
+        # when I spell it out in english suddenly I'm using MORE characters than the original expression
+        nonterminal = t.content
+        subtrees[nonterminal] ||= []
+        subtrees[nonterminal] << t
+      end
+      subtrees
+    end
+
+    # Converts the terminals of a parse tree to arrays of wordnet objects
+    def transform_tree(tree)
+      if(tree.children.empty?)
+        pos = parse_pos(tree.parent.content)
+        if pos
+          tree.content = Wordnet.search(tree.content, pos)
+        end
+      else
+        tree.children.each{|child|transform_tree(child)}
+      end
+    end
+
+    def parse_pos(nonterminal)
+      # Not 100% sure this is complete or accurate
+      case nonterminal
+        when /^N/ then :noun
+        when /^V/ then :verb
+        when /^RB/ then :adv
+        when /^J/ then :adj
+      end
+    end
+  end
+
+  extend ClassMethods
+end
