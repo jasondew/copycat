4e2a25a706c8dfefe971e8131b54db28baa1dfc8
Team JED <jed@seymour.cse.sc.edu>
2011-05-01 18:00:43 -0400

    Don't look at this commit it's just too minor

--- lib/wordnet.rb
+++ lib/wordnet.rb
@@ -104,7 +104,8 @@ module Wordnet
     BAD_SIMILARITY_POINTERS = ['!']
     def similar_word
       my_pos = {:noun => 'n', :adv => 'r', :adj => 'a', :verb => 'v'}[part_of_speech]
-      sims = pointers.select{|p|p[2] == my_pos and not BAD_SIMILARITY_POINTERS.include?(p[0])}.map { |p| Wordnet[p[1], part_of_speech]}
+      sims = pointers.select{ |p| p[2] == my_pos and not BAD_SIMILARITY_POINTERS.include?(p[0])}.
+                      map   { |p| Wordnet[p[1], part_of_speech]}
       sims << self
       sims[rand(sims.length)]
     end
